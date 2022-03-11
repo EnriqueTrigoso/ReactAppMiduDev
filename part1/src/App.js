1647b6924ee06ff7f19d656af1b1250bfe44be5b
TrOuBleDMinDl <enrique.el.capo@gmail.com>
2022-03-11 17:59:11 -0500

    primer commit

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text='primer texto' />
+      <Message text='segundo texto' />
+      <Message text='tercer texto' />
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/src/Message.js
@@ -0,0 +1,7 @@
+
+const Message = (props) => {
+    console.log(props);
+    return <h1>{props.text}</h1>
+};
+
+export default Message
